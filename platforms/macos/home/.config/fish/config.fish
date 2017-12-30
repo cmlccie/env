e30610d63bb9df077ea4cb136ffc0c48b2ed86f3
Chris Lunsford <chrlunsf@cisco.com>
2017-12-29 23:13:28 -0500

    Add fish shell to macOS

---
+++ platforms/macos/home/.config/fish/config.fish
@@ -0,0 +1,71 @@
+#echo "Loading: config.fish"
+
+if status --is-login
+	# Login Shell - Initialize Global Environment Variables
+
+    ### Ensure SHELL is set to fish
+    set -gx SHELL /usr/local/bin/fish
+
+
+	### PATH Extensions
+	## Prefixes
+	# Local Directories
+	set -gx PATH ~/dev/bin ~/.local/bin $PATH
+
+	## Suffixes
+	# Visual Studio Code
+	set -gx PATH $PATH "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+
+	### PYTHONPATH Extensions
+	set -gx PYTHONPATH ~/dev/lib $PYTHONPATH
+
+
+	### Console Configuration
+	set -gx LC_ALL en_US.UTF-8
+	set -gx LANG en_US.UTF-8
+	set -gx GREP_OPTIONS '--color auto'
+
+
+	### App Configuration
+	## direnv
+	set -gx DIRENV_LOG_FORMAT
+
+	## pip
+	#set -gx PIP_REQUIRE_VIRTUALENV true
+
+	## pipenv
+	set -gx PIPENV_VENV_IN_PROJECT 1
+    set -gx PIPENV_SHELL_FANCY 1
+
+    ## virtualenvwrapper
+    #set -gx WORKON_HOME ~/dev/envs
+    #set -gx PROJECT_HOME ~/dev/projects
+    #set -gx VIRTUALENVWRAPPER_PYTHON /usr/local/bin/python3
+
+
+end
+
+
+if status --is-interactive
+    # Interactive Shell
+
+    ### Shell Tools
+    ## pyenv
+    source (pyenv init -|psub)
+
+
+    ### Interactive Environment Variable Management
+    ## direnv
+    eval (direnv hook fish)
+
+
+else
+    # Non-Interactive Shell
+
+    ### Non-Interactive Environment Variable Management
+    ## direnv
+    eval (direnv export fish)
+
+
+end
---
+++ platforms/macos/home/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/chrlunsf/.local/share/omf/themes/bobthefish/fish_prompt.fish

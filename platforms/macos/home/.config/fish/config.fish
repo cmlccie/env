fa08d53b48ab0d86bebd5effb48944c7e283f3dd
Chris Lunsford <chrlunsf@cisco.com>
2018-02-08 22:32:52 +0700

    Customize fish powerline colors
    Use Cisco brand colors for PowerLine prompt.

--- platforms/macos/home/.config/fish/config.fish
+++ platforms/macos/home/.config/fish/config.fish
@@ -9,7 +9,7 @@ if status --is-login
 	set -gx LC_ALL en_US.UTF-8
 	set -gx LANG en_US.UTF-8
 	set -gx GREP_OPTIONS '--color auto'
-	set -gx theme_powerline_fonts yes
+	# set -gx theme_powerline_fonts yes
 	set -gx theme_nerd_fonts yes
 
 
@@ -61,6 +61,49 @@ if status --is-interactive
 
 	## pew
 	source (pew shell_config)
+
+	## powerline
+	# Color Scheme:
+	set -g theme_color_scheme user
+
+	#               light  medium dark
+	#               ------ ------ ------
+	set -l black    000000
+	set -l white	ffffff e0e0e0
+	set -l grey     666666 525252 525252
+	set -l blue     unused 0096D6 2970a6
+	set -l green    C1CD23 46A040
+	set -l purple	652D89
+	set -l red		unused E31B23 C41230
+	set -l orange   F58025
+	set -l yellow	FFE14F
+
+      set __color_initial_segment_exit     $white[1]	$red[2]		--bold
+      set __color_initial_segment_su       $white[1]	$green[2]	--bold
+      set __color_initial_segment_jobs     $white[1]	$blue[3]	--bold
+
+      set __color_path                     $grey[2]		$white[2]
+      set __color_path_basename            $grey[2]		$white[1]	--bold
+      set __color_path_nowrite             $red[3]		$white[2]
+      set __color_path_nowrite_basename    $red[3]		$white[1]	--bold
+
+      set __color_repo                     $green[1]	$grey[3]
+      set __color_repo_work_tree           $grey[2]		$white[2]	--bold
+      set __color_repo_dirty               $red[3]		$white[2]
+      set __color_repo_staged              $orange		$grey[3]
+
+      set __color_vi_mode_default          $grey[2]		$white[2]	--bold
+      set __color_vi_mode_insert           $green[2]	$white[2]	--bold
+      set __color_vi_mode_visual           $orange		$white[2]	--bold
+
+      set __color_vagrant                  $purple		$white[2]	--bold
+      set __color_k8s                      $green[2] 	$white[1]	--bold
+      set __color_username                 $grey[1]		$blue[2]	--bold
+      set __color_hostname                 $grey[1]		$blue[2]
+      set __color_rvm                      $red[2]		$white[2]	--bold
+      set __color_virtualfish              $blue[2]		$white[2]	--bold
+      set __color_virtualgo                $blue[2]		$white[2]	--bold
+
 
     ### Interactive Environment Variable Management
     ## direnv

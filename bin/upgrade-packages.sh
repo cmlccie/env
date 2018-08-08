2e1fa5b64971417351c30f750441fb65bd7d40f2
Chris Lunsford <chris@cmlccie.com>
2018-08-08 15:43:01 -0400

    Update bin/upgrade-packages.sh

--- bin/upgrade-packages.sh
+++ bin/upgrade-packages.sh
@@ -10,3 +10,8 @@ echo "==> Upgrading System Python Packages"
 pip2 install --upgrade pip setuptools
 pip3 install --upgrade pip setuptools
 pip3 install --upgrade -r ~/dev/shell/sys3-requirements.txt
+
+
+echo "==> Recreating dev3 Environment"
+pew rm dev3
+pew new -p python3 -r ~/dev/sandbox/dev3/requirements.txt dev3

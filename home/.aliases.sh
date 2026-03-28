f6d62e8595edb07a4eba0c1877db6bbeca752aa8
Chris Lunsford <cm@lunsford.io>
2026-03-28 16:11:20 -0400

    feat: add Terragrunt aliases to .aliases.sh

--- home/.aliases.sh
+++ home/.aliases.sh
@@ -2,3 +2,9 @@
 alias ll='ls -la'
 alias la='ls -la'
 alias k='kubectl'
+
+# Terragrunt
+alias tg='terragrunt'
+alias tgi='terragrunt init --non-interactive'
+alias tgp='terragrunt plan --non-interactive'
+alias tga='terragrunt apply --non-interactive'
